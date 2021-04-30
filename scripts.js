3f2ba74729db1c6f060a44ce0488ecfffb3ed4a9
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-04-30 12:31:40 +0530

    toggle icon function and header color

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 const navSlide = () =>{
-    const nav = document.querySelector('.toggle-icon')
+    const nav = document.querySelector('#toggle')
     const navResponse = document.querySelector('.nav-responsive')
     const header = document.querySelector('.container-header')
     nav.addEventListener('click',() => {
         navResponse.classList.toggle('nav-active')
         if($(window).scrollTop() < 60){
-            if(header.style.backgroundColor == "rgb(255, 255, 255)"){
-                header.style.backgroundColor = 'transparent'
-            }else{
+            if(header.style.backgroundColor == ""){
                 header.style.backgroundColor = "#fff"
+            }else{
+                header.style.backgroundColor = ""
             }
         }
     })
 }
+navSlide();
 $(window).on("scroll", function() {
     console.log('scroll');
     if($(window).scrollTop() > 80) {
@@ -23,4 +24,3 @@ $(window).on("scroll", function() {
        $(".social-link").removeClass("social-link-active")
     }
 });
-navSlide();
